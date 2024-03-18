67747de0d196e77d653b451d01a6182105e2a9cf
kyeiiih <jaydevsengineer@gmail.com>
2024-03-19 00:54:28 +0300

    Add data to bottom sheet

--- app/src/main/java/com/remotejobs/android/ui/components/BottomSheetItem.kt
+++ app/src/main/java/com/remotejobs/android/ui/components/BottomSheetItem.kt
@@ -1,9 +1,14 @@
 package com.remotejobs.android.ui.components
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.rounded.AddTask
@@ -11,11 +16,13 @@ import androidx.compose.material.icons.rounded.AttachMoney
 import androidx.compose.material.icons.rounded.RemoveRedEye
 import androidx.compose.material.icons.rounded.ShoppingBag
 import androidx.compose.material.icons.rounded.TaskAlt
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.remotejobs.android.model.Job
@@ -29,145 +36,247 @@ fun BottomSheetItem(
     Column(
         modifier = Modifier.clickable {
             onClick()
-        }
-    ) {
+        },
+
+        ) {
 
         Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth()
         ) {
             Text(
-                text = job.title, modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 3.dp)
+                text = job.title,
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(
+                        horizontal = 10.dp,
+                        vertical = 5.dp
+                    )
             )
 
             Icon(Icons.Default.Share, contentDescription = null, tint = Color.Black)
         }
 
-        Row {
-            Text(
-                text = "Ascendion",
-                fontSize = 10.sp,
-                modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 3.dp)
-            )
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
             Text(
                 text = job.company,
-                fontSize = 10.sp,
+                fontSize = 15.sp,
                 modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 3.dp)
+                    .padding(
+                        horizontal = 10.dp,
+                        vertical = 5.dp
+                    )
             )
             Text(
-                text = "1 week ago",
-                fontSize = 10.sp,
+                text = job.companyCity,
+                fontSize = 15.sp,
                 modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 3.dp)
+                    .padding(
+                        horizontal = 10.dp,
+                        vertical = 5.dp
+                    )
             )
             Text(
-                text = "Over 100 applicants",
-                fontSize = 10.sp,
+                text = job.companyCountry,
+                fontSize = 15.sp,
                 modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 3.dp)
+                    .padding(
+                        horizontal = 10.dp,
+                        vertical = 5.dp
+                    )
+            )
+            Text(
+                text = job.applications.toString(),
+                fontSize = 15.sp,
+                modifier = Modifier
+                    .padding(
+                        horizontal = 10.dp,
+                        vertical = 5.dp
+                    )
             )
         }
 
         Row {
             Icon(
                 Icons.Rounded.AttachMoney, contentDescription = null, tint = Color.Black,
+                modifier = Modifier
+                    .padding(
+                        horizontal = 8.dp,
+                        vertical = 5.dp
+                    )
             )
             Text(
                 text = "Pay: ",
-                fontSize = 10.sp,
+                fontSize = 13.sp,
+                fontWeight = FontWeight.Bold,
                 modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 3.dp)
+                    .padding(
+                        horizontal = 10.dp,
+                        vertical = 5.dp
+                    )
             )
             Text(
-                text = "100K - 180K",
-                fontSize = 10.sp,
+                text = "${job.payScaleMin}K - ${job.payScaleMax}K",
+                fontSize = 13.sp,
                 modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 3.dp)
+                    .padding(
+                        horizontal = 10.dp,
+                        vertical = 5.dp
+                    )
             )
             Text(
-                text = "Remote",
-                fontSize = 10.sp,
+                text = job.type,
+                fontSize = 13.sp,
                 modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 3.dp)
+                    .padding(
+                        horizontal = 10.dp,
+                        vertical = 5.dp
+                    )
             )
             Text(
-                text = "Full-time",
-                fontSize = 10.sp,
+                text = job.duration,
+                fontSize = 13.sp,
                 modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 3.dp)
+                    .padding(
+                        horizontal = 10.dp,
+                        vertical = 5.dp
+                    )
             )
             Text(
-                text = "Mid-Senior level",
-                fontSize = 10.sp,
+                text = job.experienceLevel,
+                fontSize = 13.sp,
                 modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 3.dp)
+                    .padding(
+                        horizontal = 10.dp,
+                        vertical = 5.dp
+                    )
             )
         }
 
         Row {
             Icon(
                 Icons.Rounded.ShoppingBag, contentDescription = null, tint = Color.Black,
+                modifier = Modifier
+                    .padding(
+                        horizontal = 8.dp,
+                        vertical = 5.dp
+                    )
             )
             Text(
                 text = "Company Size: ",
-                fontSize = 10.sp,
+                fontSize = 13.sp,
+                fontWeight = FontWeight.Bold,
                 modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 3.dp)
+                    .padding(
+                        horizontal = 10.dp,
+                        vertical = 5.dp
+                    )
             )
             Text(
-                text = "10-20 employees",
-                fontSize = 10.sp,
+                text = "${job.noOfEmployeesLowBound} - ${job.noOfEmployeesHighBound} employees",
+                fontSize = 13.sp,
                 modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 3.dp)
+                    .padding(
+                        horizontal = 10.dp,
+                        vertical = 5.dp
+                    )
             )
             Text(
-                text = "Technology, Information and Internet",
-                fontSize = 10.sp,
+                text = job.companyType,
+                fontSize = 13.sp,
                 modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 3.dp)
+                    .padding(
+                        horizontal = 10.dp,
+                        vertical = 5.dp
+                    )
             )
         }
 
         Row {
             Icon(
                 Icons.Rounded.TaskAlt, contentDescription = null, tint = Color.Black,
+                modifier = Modifier
+                    .padding(
+                        horizontal = 8.dp,
+                        vertical = 5.dp
+                    )
             )
             Text(
                 text = "Skills: ",
-                fontSize = 10.sp,
+                fontSize = 13.sp,
+                fontWeight = FontWeight.Bold,
                 modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 3.dp)
+                    .padding(
+                        horizontal = 10.dp,
+                        vertical = 5.dp
+                    )
             )
-            Text(
-                text = "Oracle Database",
-                fontSize = 10.sp,
-                modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 3.dp)
-            )
-            Text(
-                text = "SQL",
-                fontSize = 10.sp,
-                modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 3.dp)
-            )
+            job.skills?.get(0)?.let {
+                Text(
+                    text = it,
+                    fontSize = 13.sp,
+                    modifier = Modifier
+                        .padding(
+                            horizontal = 10.dp,
+                            vertical = 5.dp
+                        )
+                )
+            }
+            job.skills?.get(1)?.let {
+                Text(
+                    text = it,
+                    fontSize = 13.sp,
+                    modifier = Modifier
+                        .padding(
+                            horizontal = 10.dp,
+                            vertical = 5.dp
+                        )
+                )
+            }
+            job.skills?.get(2)?.let {
+                Text(
+                    text = it,
+                    fontSize = 13.sp,
+                    modifier = Modifier
+                        .padding(
+                            horizontal = 10.dp,
+                            vertical = 5.dp
+                        )
+                )
+            }
+
         }
 
         Row {
             Icon(
                 Icons.Rounded.RemoveRedEye, contentDescription = null, tint = Color.Black,
+                modifier = Modifier
+                    .padding(
+                        horizontal = 8.dp,
+                        vertical = 5.dp
+                    )
             )
             Text(
                 text = "Views: ",
-                fontSize = 10.sp,
+                fontSize = 13.sp,
+                fontWeight = FontWeight.Bold,
                 modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 3.dp)
+                    .padding(
+                        horizontal = 10.dp,
+                        vertical = 5.dp
+                    )
             )
             Text(
-                text = "87",
-                fontSize = 10.sp,
+                text = job.views.toString(),
+                fontSize = 13.sp,
                 modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 3.dp)
+                    .padding(
+                        horizontal = 10.dp,
+                        vertical = 5.dp
+                    )
             )
 
         }
@@ -175,20 +284,45 @@ fun BottomSheetItem(
         Row {
             Icon(
                 Icons.Rounded.AddTask, contentDescription = null, tint = Color.Black,
+                modifier = Modifier
+                    .padding(
+                        horizontal = 8.dp,
+                        vertical = 5.dp
+                    )
             )
             Text(
                 text = "Applications: ",
-                fontSize = 10.sp,
+                fontSize = 13.sp,
+                fontWeight = FontWeight.Bold,
                 modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 3.dp)
+                    .padding(
+                        horizontal = 10.dp,
+                        vertical = 5.dp
+                    )
             )
             Text(
-                text = "87",
-                fontSize = 10.sp,
+                text = job.applications.toString(),
+                fontSize = 13.sp,
                 modifier = Modifier
-                    .padding(horizontal = 5.dp, vertical = 3.dp)
+                    .padding(
+                        horizontal = 10.dp,
+                        vertical = 5.dp
+                    )
             )
 
+        }
+
+        Spacer(
+            modifier =
+            Modifier
+                .height(20.dp)
+        )
+
+        Button(
+            onClick = { onClick() }, modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text("Apply Now")
         }
 
     }
